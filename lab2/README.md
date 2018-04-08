6d506a6f7135aa5c79e5029e1895400170cfb609
MihaiGaidau <MISKA@DESKTOP-B1BKFFB.localdomain>
2018-04-08 14:13:22 +0300

    secon lab

---
+++ lab2/README.md
@@ -0,0 +1,21 @@
+# REPORT
+
+**Objective:**
+
+* Get familiar with MVC pattern
+
+Framework: **ASP.NET**
+ASP.NET MVC gives you a powerful, patterns-based way to build dynamic websites that enables a clean separation of concerns and that gives you full control over markup for enjoyable, agile development. ASP.NET MVC includes many features that enable fast, TDD-friendly development for creating sophisticated applications that use the latest web standards.
+ASP.NET MVC allows software developers to build a web application as a composition of three roles: Model, View and Controller
+1. *Model* - represents the state of a particular aspect of the application
+2. *View* - accepts necessary information from the controller and renders a user interface to display that information
+3. *Controller* - handles interactions and updates the model to reflect a change in state of the application, and then passes information to the view
+
+In this laboratory work I optained experinece with working with a MVC pattern. Fist of all, I integrated my first laboratory with the MVC, In order to ot to write to much code, I used the ASP.NET feature of layers, that create a file, where is some code, that I can insert in another page, using just a link.
+
+Another feature of the MVC is "bundles", that creates a virtual link of css or js files that can be included by virtual link in every file
+
+**References**
+
+1.Presentation from WebTechnology courses. Antohi Ionel
+2. [Wikipedia ASP.NET MVC](https://en.wikipedia.org/wiki/ASP.NET_MVC)
---
+++ lab2/eUseControl/eUseControl.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace eUseControl.Web
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap.min.css",
+                new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/categories/css").Include("~/Style/categories.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/reg-log-forms/css").Include("~/Style/reg-log-forms.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/style/css").Include("~/Style/style.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/view-profile/css").Include("~/Style/view-profile.css", new CssRewriteUrlTransform()));
+            bundles.Add(new ScriptBundle("~/bundles/menu-resize/js").Include("~/Scripts/menu-resize.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquerry/js").Include("~/Scripts/jquerry-3.3.1.slim.js"));
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new ScriptBundle("~/bundles/tether/js").Include("~/Scripts/tether.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include("~/Scripts/bootstrap.min.js"));
+
+        }
+    }
+}
