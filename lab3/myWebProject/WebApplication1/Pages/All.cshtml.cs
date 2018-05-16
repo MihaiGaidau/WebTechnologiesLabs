f88af2d9c98481976487b348e7ede29f46dbd352
MihaiGaidau <MISKA@DESKTOP-B1BKFFB.localdomain>
2018-05-16 13:32:11 +0300

    llab

---
+++ lab3/myWebProject/WebApplication1/Pages/All.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Pages
+{
+    public class AllModel : PageModel
+    {
+        private IRestaurantData _restaurants;
+        public static IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public AllModel(IRestaurantData restaurants)
+        {
+            _restaurants = restaurants;
+        }
+        public void OnGet()
+        {
+            Restaurants = _restaurants.GetAll();
+        }
+    }
+}
---
+++ lab3/myWebProject/WebApplication1/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private IRestaurantData _restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+        public EditModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Restaurant = _restaurantData.Get(id);
+            if (Restaurant == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return Page();
+        }
+    }
+}
